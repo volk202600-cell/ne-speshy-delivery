99fceef58c3e688ee81b421a53d4f1d710b7423d
volk202600-cell <volk202600@gmail.com>
2026-02-01 02:17:30 +0300

    Create menu.ts

---
+++ data/menu.ts
@@ -0,0 +1,24 @@
+export type MenuItem = {
+  id: string;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+};
+
+export const menu: MenuItem[] = [
+  {
+    id: "burger-classic",
+    name: "Бургер Класичний",
+    price: 120,
+    description: "Яловичина, сир, салат, соус",
+    image: "/burger1.jpg",
+  },
+  {
+    id: "burger-bbq",
+    name: "Бургер BBQ",
+    price: 140,
+    description: "BBQ соус, бекон, сир",
+    image: "/burger2.jpg",
+  },
+];
