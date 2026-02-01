79301f016d8901a8fd470b280b5e2fa5e73ec94e
volk202600-cell <volk202600@gmail.com>
2026-02-01 05:39:20 +0300

    Create AdminClient component for admin interface

---
+++ app/admin/AdminClient.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function AdminClient() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Адмінка</h1>
+      <p>Тут буде додавання / видалення меню</p>
+    </div>
+  )
+}
